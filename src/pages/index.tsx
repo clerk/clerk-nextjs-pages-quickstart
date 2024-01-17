8465c580ec061e677b6e932359bcbf9b635e7883
Roy Anger <roy@royanger.com>
2024-01-17 01:53:50 -0500

    Updated for v5 and to match updated Quickstart

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
-import { UserButton } from "@clerk/nextjs";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <>
-      <header>
-        <UserButton afterSignOutUrl="/" />
-      </header>
-      <div>Your page's content can go here.</div>
+      <SignedOut>
+        <SignInButton />
+        <p>This content is public. Only signed out users can see this.</p>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+        <p>This content is private. Only signed in users can see this.</p>
+      </SignedIn>
     </>
   );
 }
